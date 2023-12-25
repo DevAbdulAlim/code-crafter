80a7c75f3648a525c7475cecfe3e0beadcd39056
Abdul Alim <aa.abdulalim13@gmail.com>
2023-12-25 19:15:21 +0600

    added lesson actions

--- components/admin/categories/table.tsx
+++ components/admin/categories/table.tsx
@@ -13,6 +13,7 @@ export default async function Table({ data }: { data: any }) {
                 key={item.id}
                 className="w-full p-4 mb-2 bg-white rounded-md"
               >
+                <p>{item.id}</p>
                 <p>{item.name}</p>
                 <div className="flex justify-between mt-2">
                   <ButtonLink
@@ -31,6 +32,9 @@ export default async function Table({ data }: { data: any }) {
             <thead className="text-sm font-normal text-left rounded-lg">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                  ID
+                </th>
+                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
                   Name
                 </th>
               </tr>
@@ -41,6 +45,7 @@ export default async function Table({ data }: { data: any }) {
                   key={item.id}
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
                 >
+                  <td className="px-3 py-3 whitespace-nowrap">{item.id}</td>
                   <td className="px-3 py-3 whitespace-nowrap">{item.name}</td>
 
                   <td className="py-3 pl-6 pr-3 whitespace-nowrap">
---
+++ lib/actions/lessonActions.ts
@@ -0,0 +1,129 @@
+import prisma from "@/config/prisma";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+
+const LessonSchema = z.object({
+  LessonId: z.number(),
+  title: z.string(),
+  description: z.string().optional(),
+  courseId: z.string(),
+});
+
+const LessonParser = (formData: FormData) =>
+  LessonSchema.safeParse({
+    LessonId: formData.get("LessonId"),
+    title: formData.get("title"),
+    description: formData.get("description") || undefined,
+    courseId: formData.get("courseId"),
+  });
+
+const createLesson = async (formData: FormData) => {
+  try {
+    const parse = LessonParser(formData);
+
+    if (!parse.success) {
+      return { message: `Failed to create lesson: ${parse.error.message}` };
+    }
+
+    const data = parse.data;
+
+    await prisma.lesson.create({
+      data,
+    });
+
+    revalidatePath("/admin/lessons"); // Ensure revalidatePath is implemented
+    return { message: "Create operation completed successfully" };
+  } catch {
+    return { message: "Database Error: Failed to Create Lesson" };
+  }
+};
+
+const updateLesson = async (formData: FormData, lessonId: string) => {
+  try {
+    const parse = LessonParser(formData);
+
+    if (!parse.success) {
+      return { message: `Failed to update lesson: ${parse.error.message}` };
+    }
+
+    const data = parse.data;
+
+    await prisma.lesson.update({
+      where: {
+        id: lessonId,
+      },
+      data,
+    });
+
+    revalidatePath("/admin/lessons"); // Ensure revalidatePath is implemented
+    return { message: "Update operation completed successfully" };
+  } catch {
+    return { message: "Database Error: Failed to Update Lesson" };
+  }
+};
+
+const deleteLesson = async (lessonId: string) => {
+  try {
+    await prisma.lesson.delete({
+      where: {
+        id: lessonId,
+      },
+    });
+
+    revalidatePath("/admin/lessons"); // Ensure revalidatePath is implemented
+    return { message: "Delete operation completed successfully" };
+  } catch {
+    return { message: "Database Error: Failed to Delete Lesson" };
+  }
+};
+
+const getAllLessons = async (
+  skip: number,
+  take: number,
+  orderBy: object,
+  where: object
+) => {
+  try {
+    const [lessons, totalItems] = await prisma.$transaction([
+      prisma.lesson.findMany({
+        skip: skip,
+        take: take,
+        orderBy: orderBy,
+        where: where,
+      }),
+      prisma.lesson.count({
+        where: where,
+      }),
+    ]);
+    return {
+      message: "Retrieved all lessons successfully",
+      data: [lessons, totalItems],
+    };
+  } catch {
+    return { message: "Database Error: Failed to Retrieve Lessons" };
+  }
+};
+
+const getLessonById = async (lessonId: string) => {
+  try {
+    const singleLesson = await prisma.lesson.findUnique({
+      where: {
+        id: lessonId,
+      },
+    });
+    return {
+      message: "Retrieved lesson successfully",
+      data: singleLesson,
+    };
+  } catch {
+    return { message: "Database Error: Failed to Retrieve Lesson" };
+  }
+};
+
+export {
+  createLesson,
+  updateLesson,
+  deleteLesson,
+  getAllLessons,
+  getLessonById,
+};
